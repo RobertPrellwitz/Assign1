a8714c7a6a0495c28fdbc4c3d88c51fb71d22f8c
Robert Prellwitz <robert.prellwitz@summaft.com>
2021-09-15 16:49:41 -0500

    minor updates to deal with whitespace when reading fiel

--- Assignment1/src/Assignment1/TextTable.java
+++ Assignment1/src/Assignment1/TextTable.java
@@ -14,24 +14,29 @@ public class TextTable {
 	public void loadTableFromFile(String fileName) throws IOException  {
 		Scanner loadFile = null;  //// = new Scanner(new FileReader());
 
-		loadFile =new Scanner(new FileReader("C:\\Users\\rprel\\APC 390\\Assignment1\\Assignment1\\data\\" +fileName));
-		//// loadFile = new Scanner(new FileReader(fileName));
+		/// loadFile =new Scanner(new FileReader("C:\\Users\\rprel\\APC 390\\Assignment1\\Assignment1\\data\\" +fileName));
+		/// loadFile = new Scanner(new FileReader("data/" +fileName));
+		loadFile = new Scanner(new FileReader(fileName));
 		String input ;
 		header = loadFile.nextLine();
 		while (loadFile.hasNext()) {
 			input = loadFile.nextLine();
+			boolean isEmpty = (input == null || input.trim().isEmpty());
+			if(!isEmpty){
 			String[] array = input.split("\t", 3);
 			//String[] array = input.split(" ",3);
 			table[counter] = new TableRow(array[0], array[1], array[2]);
 			counter++;
+			}
 		}
 		loadFile.close();
 	}
 
 	public void saveTableToFile(String fileName) throws FileNotFoundException {
 	////PrintWriter fileOutput = new PrintWriter("C:\\Users\\rprel\\APC 390\\Assignment1\\Assignment1\\output\\" + fileName);
-		String path = new File("C:\\Users\\rprel\\APC 390\\Assignment1\\Assignment1\\output\\" + fileName).toString();
-		System.out.println("\\src\\"+path);
+	///	String path = new File("C:\\Users\\rprel\\APC 390\\Assignment1\\Assignment1\\output\\" + fileName).toString();
+	///	System.out.println("\\src\\"+path);
+	
 	PrintWriter fileOutput = new PrintWriter(fileName);
 
 	String number, name, score;
